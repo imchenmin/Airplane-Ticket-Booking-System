41ce5f80921eb0c2210c2b34afe8b21a4573d0a9
dounidian <zbcmb@outlook.com>
2017-04-22 15:39:14 +0800

    Ken create this project.

---
+++ src/ControlFlow.java
@@ -0,0 +1,16 @@
+
+public class ControlFlow {
+/**
+ * 这个类是Airplane-Ticket-Booking-System 项目的入口。
+ * 功能：展现版本信息、初始化，调配其他类。
+ * @param args
+ */
+	public static void main(String args[]){
+		
+	}
+	public static void version(){
+		System.out.println("这是一个小组项目。目前版本0.01");
+		//注释：为了载入github。
+		System.out.println("HH.");
+	}
+}
