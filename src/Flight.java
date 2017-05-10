8fcec22c382f7b60e7282045e2064f3c225baa65
luotianzuo <jj@192.168.1.104>
2017-05-10 15:43:13 +0800

    先交点上去吧

---
+++ src/Flight.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Flight {
+        private String FlightID;
+        private String startTime;
+        private String arrivalTime;
+        private String startCity;
+        private String arrivalCity;
+        private String departureDate;
+        private int price;
+        private int currentPassengers;
+        private int seatCapacity;
+        private String flightStatus;
+        private ArrayList<Integer> passengerIDs = new ArrayList<>();
+        
+}
